75d33a65edb15f6c6eaa4f12d0daa6f06eb53ed7
Vinh Nguyen <toiyeudanang1998@gmail.com>
2023-10-09 20:31:23 +0700

    ada4

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const axios = require('axios');
+
 
 const app = express();
 
@@ -9,6 +11,30 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', (req, res) => {
+    let config = {
+        method: 'get',
+        url: 'https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=ho-chi-minh/com-ba-ghien-nguyen-van-troi',
+        headers: {
+            "Cache-Control": "no-cache",
+            "Content-Type": "application/x-www-form-urlencoded",
+            'X-FoodyAccessToken': '',
+            'X-Foody-Api-Version': '1',
+            'X-Foody-App-Type': '1004',
+            'X-Foody-Client-Id': '',
+            'X-Foody-Client-Language': 'vi',
+            'X-Foody-Client-Type': '1',
+            'X-Foody-Client-Version': '3.0.0',
+            'X-Sap-Ri': 'ef6c216559a26b6011140d38562609a2f46c5111f0762022'
+        }
+    };
+
+    axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     console.log(13);
     res.json({ "message": "hello world" });
 });
