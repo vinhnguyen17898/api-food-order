2e84474edc6384bc8db4a25a1cc09ed4ec151e4a
Vinh Nguyen <toiyeudanang1998@gmail.com>
2023-10-09 21:17:47 +0700

    ada7

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/restaurantId', (req, res) => {
+async function axiosTest() {
     let config = {
         method: 'get',
         url: 'https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=ho-chi-minh/com-ba-ghien-nguyen-van-troi',
@@ -27,15 +27,40 @@ app.get('/restaurantId', (req, res) => {
 
         }
     };
+    const response = await axios.request(config)
+    console.log(response.data);
+    return response.data;
+}
 
-    axios.request(config)
-        .then((response) => {
-            console.log(1321313);
-            res.status(200).json(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+app.get('/restaurantId',  async (req, res) => {
+    var data = await axiosTest();
+    console.log("1_" + data);
+    // let config = {
+    //     method: 'get',
+    //     url: 'https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=ho-chi-minh/com-ba-ghien-nguyen-van-troi',
+    //     headers: {
+    //         'X-FoodyAccessToken': '',
+    //         'X-Foody-Api-Version': '1',
+    //         'X-Foody-App-Type': '1004',
+    //         'X-Foody-Client-Id': '',
+    //         'X-Foody-Client-Language': 'vi',
+    //         'X-Foody-Client-Type': '1',
+    //         'X-Foody-Client-Version': '3.0.0',
+    //         'X-Sap-Ri': 'ef6c216559a26b6011140d38562609a2f46c5111f0762022',
+
+    //     }
+    // };
+
+    // axios.request(config)
+    //     .then((response) => {
+    //         console.log(response.data);
+    //         data = response.data.restaurant_id;
+    //         return data;
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    return res.status(200).json(data);
 });
 
 const port = 8080;
