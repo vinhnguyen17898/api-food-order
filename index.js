7daae11ab124a6e3c82aa2706490f0eda6c9e05d
Vinh Nguyen <toiyeudanang1998@gmail.com>
2023-10-10 20:21:38 +0700

    ada9

--- index.js
+++ index.js
@@ -11,55 +11,34 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-async function axiosTest() {
-    let config = {
-        method: 'get',
-        url: 'https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=ho-chi-minh/com-ba-ghien-nguyen-van-troi',
-        headers: {
-            'X-FoodyAccessToken': '',
-            'X-Foody-Api-Version': '1',
-            'X-Foody-App-Type': '1004',
-            'X-Foody-Client-Id': '',
-            'X-Foody-Client-Language': 'vi',
-            'X-Foody-Client-Type': '1',
-            'X-Foody-Client-Version': '3.0.0',
-            'X-Sap-Ri': 'ef6c216559a26b6011140d38562609a2f46c5111f0762022',
-
-        }
-    };
-    const response = await axios.request(config)
-    console.log(response.data);
-    return response.data;
-}
-
 app.get('/',  async (req, res) => {
-    var data = await axiosTest();
-    console.log("1_" + data);
-    // let config = {
-    //     method: 'get',
-    //     url: 'https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=ho-chi-minh/com-ba-ghien-nguyen-van-troi',
-    //     headers: {
-    //         'X-FoodyAccessToken': '',
-    //         'X-Foody-Api-Version': '1',
-    //         'X-Foody-App-Type': '1004',
-    //         'X-Foody-Client-Id': '',
-    //         'X-Foody-Client-Language': 'vi',
-    //         'X-Foody-Client-Type': '1',
-    //         'X-Foody-Client-Version': '3.0.0',
-    //         'X-Sap-Ri': 'ef6c216559a26b6011140d38562609a2f46c5111f0762022',
+    return res.status(200).json("Hello");
+});
 
-    //     }
-    // };
 
-    // axios.request(config)
-    //     .then((response) => {
-    //         console.log(response.data);
-    //         data = response.data.restaurant_id;
-    //         return data;
-    //     })
-    //     .catch((error) => {
-    //         console.log(error);
-    //     });
+app.get('/restaurantDetail',  async (req, res) => {
+    console.log("da vao");
+    console.log();
+    const getRestaurantDetail = async () =>{
+        let config = {
+            method: 'get',
+            url: 'https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=' + req.query.restaurantName,
+            headers: {
+                'X-FoodyAccessToken': '',
+                'X-Foody-Api-Version': '1',
+                'X-Foody-App-Type': '1004',
+                'X-Foody-Client-Id': '',
+                'X-Foody-Client-Language': 'vi',
+                'X-Foody-Client-Type': '1',
+                'X-Foody-Client-Version': '3.0.0',
+                'X-Sap-Ri': 'ef6c216559a26b6011140d38562609a2f46c5111f0762022',
+    
+            }
+        };
+        const response = await axios.request(config);
+        return response.data;
+    }
+    var data = await getRestaurantDetail();
     return res.status(200).json(data);
 });
 
